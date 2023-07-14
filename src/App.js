e380f02374e70bce0082de7c18446234391d0380
IgnacioGarciaReyna <ignaciogarciareyna@live.com>
2023-07-14 01:10:48 -0300

    Some adjustments

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import React from "react";
 
 function App() {
-  const [firstTeam, setFirstTeam] = useState(["na"]);
-  const [secondTeam, setSecondTeam] = useState([]);
+  const [players, setPlayers] = useState([]);
 
   const addPlayer = (e) => {
     e.preventDefault();
     console.log(e.target.name.value);
-    const newPlayer = e.target.name.value;
-    setFirstTeam([...firstTeam, newPlayer]);
+    const newPlayer = {
+      id: e.target.name.value,
+      name: e.target.name.value,
+      skill: 0,
+      goalkeeper: false,
+    };
+    setPlayers([...players, newPlayer]);
   };
 
   return (
@@ -17,35 +21,17 @@ function App() {
       <h1>Fulbito</h1>
       <form onSubmit={addPlayer}>
         <input type="text" name="name" />
-        <select>
-          <option value="0" key="0">
-            -- Seleccionar Equipo --
-          </option>
-          <option value="1" key="1">
-            Equipo 1
-          </option>
-          <option value="2" key="2">
-            Equipo 2
-          </option>
-        </select>
         <button type="submit" name="submit">
           Cargar Jugador
         </button>
       </form>
       <div>
         <ol>
-          {firstTeam.map((player) => (
-            <li key={player}>{player}</li>
+          {players.map((player) => (
+            <li key={player.id}>{player.name}</li>
           ))}
         </ol>
       </div>
-      {/* <div>
-        <ol>
-          {secondTeam.map((player) => (
-            <li>{player}</li>
-          ))}
-        </ol>
-      </div> */}
     </div>
   );
 }
