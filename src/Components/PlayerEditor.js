158f729ee4f14f80d41a07f9b99766ed3fbcef51
IgnacioGarciaReyna <ignaciogarciareyna@live.com>
2023-07-14 20:53:42 -0300

    A few styles were added to the playereditor

--- src/Components/PlayerEditor.js
+++ src/Components/PlayerEditor.js
@@ -3,22 +3,26 @@ import React from "react";
 const PlayerEditor = ({ player, deletePlayer }) => {
   const setSkill = (e) => {
     e.preventDefault();
-
     player.skill = e.target.value;
-    console.log(player);
   };
 
-  const handleDeletePlayer = (id) => {
-    deletePlayer(id);
+  const handleDeletePlayer = () => {
+    deletePlayer(player.id);
+  };
+
+  const changePosition = () => {
+    player.goalkeeper = !player.goalkeeper;
   };
 
   return (
-    <li key={player.id}>
+    <li className="player-editor" key={player.id}>
       <p>{player.name}</p>
+      <p>Skill:</p>
       <input type="number" name="skill" min="0" max="5" onChange={setSkill} />
-      <p>Skill: {player.skill}</p>
-      {player.goalkipper ? <p>Arquero</p> : <p>Jugador</p>}
-      <button type="button" onClick={() => handleDeletePlayer(player.id)}>
+      <p onClick={changePosition}>
+        {player.goalkeeper ? "Arquero" : "Jugador"}
+      </p>
+      <button type="button" onClick={handleDeletePlayer}>
         Delete
       </button>
     </li>
